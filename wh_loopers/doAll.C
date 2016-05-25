c6bf46b75b47e037e62966a503fabc411a01afcc
Mia Liu <miaoyuan.liu0@gmail.com>
2016-05-25 04:20:30 -0700

    delete more files

--- wh_loopers/doAll.C
+++
@@ -1,8 +0,0 @@
-{
-
-  gROOT->ProcessLine(".L ScanChain.C+");
-
-  TChain *ch = new TChain("t"); 
-  ch->Add("/nfs-7/userdata/mliu/onelepbabies/V00-00-02/ttbar_powheg_pythia8_ext3_25ns.root");
-  ScanChain(ch); 
-}
