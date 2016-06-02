382fd8321c5723b609c63ef53e79fb6804587139
Mia Liu <miaoyuan.liu0@gmail.com>
2016-06-02 08:27:55 -0700

    updates for 74

--- sharedCode/WHSelection.h
+++ sharedCode/WHSelection.h
@@ -7,6 +7,7 @@ bool passPreselection(std::string selection);
 bool pass2lPreselection();
 bool passBaseline( std::string selection);
 bool passSR( std::string selection );
+bool passcutflow( std::string selection );
 bool passRegion(std::string selection);
 bool passStudyRegion(std::string selection);
 bool pass1lCR( std::string selection );
@@ -14,6 +15,7 @@ bool pass2lCR( std::string selection );
 bool passmbbCR( std::string selection );
 bool passbin( std::string selection);
 float event_weight(std::string selection);
+bool passISR();
 // the following functions are used to catergorize 2l CR estimation.
 // following functions are used for gen level studies.
 std::pair<vector<int>,vector<int> >btaggedjets();
