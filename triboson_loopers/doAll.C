95ea8908d42e83b3099fe9142b68dac7bf5326b1
Mia Liu <miaoyuan.liu0@gmail.com>
2017-06-18 20:52:02 -0700

    remove scripts not used

--- triboson_loopers/doAll.C
+++
@@ -1,8 +0,0 @@
-{
-
-  gROOT->ProcessLine(".L ScanChain.C+");
-
-  TChain *ch = new TChain("t"); 
-  ch->Add("/nfs-7/userdata/mliu/tupler_babies/merged/onelepbabymaker/moriond2017.v4/output/data_single_electron_Run2016B_MINIAOD_23Sep2016-v3.root");
-  ScanChain(ch); 
-}
