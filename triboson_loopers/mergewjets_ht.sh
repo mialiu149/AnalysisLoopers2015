052dd4c465603ec1f0e7bb7195edf5fe1163538f
Mia Liu <miaoyuan.liu0@gmail.com>
2017-09-27 03:49:30 -0700

    delete scripts from wh

--- triboson_loopers/mergewjets_ht.sh
+++
@@ -1,7 +0,0 @@
-echo "merging stitched w+jets samples"
-selection="presel_ss_ee_isotrackveto_loosebtag"
-. mergewjets_cmd.sh $selection
-selection="presel_ss_mm_isotrackveto_loosebtag"
-. mergewjets_cmd.sh $selection
-selection="presel_ss_em_isotrackveto_loosebtag"
-. mergewjets_cmd.sh $selection
